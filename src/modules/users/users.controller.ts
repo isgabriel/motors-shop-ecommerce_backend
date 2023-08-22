168b87c319b33dea0dec50872af3cfbe158c9d66
Maksuel Nascimento <mkdias_nascimento@hotmail.com>
2023-08-22 07:26:05 -0300

    fix: add ownership check for user account

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,21 +1,14 @@
 /* eslint-disable prettier/prettier */
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  HttpCode,
-  Param,
-  Patch,
-  Post,
-  Request,
-  UseGuards,
-} from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post, Req, Request, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 
+interface ReqWithUser extends Request {
+  user: User;
+}
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
@@ -45,14 +38,14 @@ export class UsersController {
 
   @Patch(':id')
   @UseGuards(JwtAuthGuard)
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(id, updateUserDto);
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto, @Req() req: ReqWithUser) {
+    return this.usersService.update(req.user.id, id, updateUserDto);
   }
 
   @Delete(':id')
   @UseGuards(JwtAuthGuard)
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(id);
+  remove(@Param('id') id: string, @Req() req: ReqWithUser) {
+    return this.usersService.remove(req.user.id, id);
   }
 
   @HttpCode(200)
@@ -63,10 +56,7 @@ export class UsersController {
   }
 
   @Patch('resetPassword/:token')
-  async resetPassword(
-    @Param('token') token: string,
-    @Body('password') password: string,
-  ) {
+  async resetPassword(@Param('token') token: string, @Body('password') password: string) {
     await this.usersService.resetPassword(password, token);
     return { message: 'Senha alterada com sucesso!' };
   }
